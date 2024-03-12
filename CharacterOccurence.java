a98ce7ff2f29ebb7b54a73a0d40b34e6de871037
Pranavi ranjan <pranavi.1995.ranjan@gmail.com>
2024-03-12 23:52:25 +0530

    Modified CharacterOccurence.java

--- CharacterOccurence.java
+++ CharacterOccurence.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 public class CharacterOccurence{
+	
 	public static void main(String[] args){
+	
         Scanner input=new Scanner(System.in);
         System.out.println("Enter any String:");
-	String str=input.next();
-	getCharOccurence(str,'a');
+		String str=input.next();
+		getCharOccurence(str,'a');
 	}
-
-public static void getCharOccurence(String str,char val){
-	int count=0;
-	for(char c:str.toCharArray()){
-		if(c==val){
-			count++;
+	
+	public static void getCharOccurence(String str,char val){
+		int count=0;
+		for(char c:str.toCharArray()){
+			if(c==val){
+				count++;
 			}
 		}
-	System.out.println(val+":"+count);
+		System.out.println(val+":"+count);
 	}
-
-
-
-
-
 }

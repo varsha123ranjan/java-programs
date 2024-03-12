1dbfa49c316452e0bc4c5241c1afacce8938d431
Pranavi ranjan <pranavi.1995.ranjan@gmail.com>
2024-03-13 00:15:54 +0530

    Fixed alinment issue

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,15 +1,13 @@
 public class SwapTwoNumbers{
-	public static void main(String[] arsg){
+
+    public static void main(String[] arsg){
 	int number1=10;
 	int number2=20;
-	
 	number1=number1+number2;//30
 	number2=number1-number2;//30-20=10
 	number1=number1-number2;//30-10
-	
-	
-System.out.println("number1: "+number1);
-System.out.println("number2: "+number2);
-	}
+        System.out.println("number1: "+number1);
+	System.out.println("number2: "+number2);
+  }
 
 }
